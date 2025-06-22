da539728d8155f1089499c2bd74965dcd0bb44d2
Wei Yang <weiyang2048@gmail.com>
2025-06-22 18:32:05 -0500

    Refactor dashboard initialization: Moved Hydra instance clearing to the end of the initialization process in `ETFs - Macro Views.py` and `Sectors - Micro Views.py` for improved clarity and stability.

--- src/dashboard/ETFs - Macro Views.py
+++ src/dashboard/ETFs - Macro Views.py
@@ -2,10 +2,7 @@ if __name__ == "__main__":
     from src.dashboard.create_page import setup_page, show_market_performance
     import hydra
 
-    try:
-        hydra.core.global_hydra.GlobalHydra.instance().clear()
-    except:
-        pass
+
 
     with hydra.initialize(version_base=None, config_path="../../conf"):
         config = hydra.compose(
@@ -16,5 +13,7 @@ if __name__ == "__main__":
                 # "~tickers.insurance_stocks",
             ],
         )
+        hydra.core.global_hydra.GlobalHydra.instance().clear()
+
 
     show_market_performance(config["tickers"], config["portfolio"], config["dashboard_layout"])
--- src/dashboard/pages/Sectors - Micro Views.py
+++ src/dashboard/pages/Sectors - Micro Views.py
@@ -4,15 +4,12 @@ import hydra
 
 if __name__ == "__main__":
 
-    try:
-        hydra.core.global_hydra.GlobalHydra.instance().clear()
-    except:
-        pass
 
     with hydra.initialize(version_base=None, config_path="../../../conf"):
         config = hydra.compose(
             config_name="config",
             overrides=["+dashboard_layout=Sectors", "portfolio=sectors_micro"],
         )
+        hydra.core.global_hydra.GlobalHydra.instance().clear()
 
     show_market_performance(config["tickers"], config["portfolio"], config["dashboard_layout"])
