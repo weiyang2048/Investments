53b19bcdc05e8a37b03be7e21603167c6ec891b2
Wei Yang <weiyang2048@gmail.com>
2025-06-29 16:24:42 -0500

    Update performance plot in `viz.py`: Refined the ALT annotation formatting for improved clarity in visual data representation. This change enhances the overall readability of performance metrics

--- src/viz.py
+++ src/viz.py
@@ -77,7 +77,7 @@ def create_performance_plot(
             + "<span style='color: snow; opacity: 0.3'>|</span>"
             + f"{"<span style='color: snow; opacity: 0.3'>|</span>".join([f"<span style='color: {colors_dict[symbol]}'>{ratio*100:.0f}</span>{'<span style="color: snow; opacity: 0.3">|</span><br>' if i!=0 and i%9==0 else ''}" for i,symbol, ratio in zip(range(len(symbols)),symbols, stats.ratios)])}"
             + f"{'<span style="color: snow; opacity: 0.3">|</span><br>' if len(symbols) % 10 != 0 else ''}"
-            + f"ALT: {"+" if stats.alternative_return > 0 else "-"} {abs(stats.alternative_return):.2%}, <span style='color: violet; '> {stats.weighted_mean_std(stats.ratios)[1]*100:.2f}</span>"
+            + f"<br>ALT: {"+" if stats.alternative_return > 0 else "-"} {abs(stats.alternative_return):.2%}, <span style='color: violet; '> {stats.weighted_mean_std(stats.ratios)[1]*100:.2f}</span>"
         )
         fig.add_annotation(
             x=min(df_normalized["Date"]),
