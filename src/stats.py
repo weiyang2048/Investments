915d449680bcd2472192a076510f670f832a4619
Wei Yang <weiyang2048@gmail.com>
2025-09-12 00:29:03 -0400

    Update configuration files and enhance performance metrics
    - Updated `main.yaml` and `regional.yaml` to improve ETF organization and clarity by adding comments and removing obsolete entries.
    - Enhanced `References.yaml` with additional fund manager links for better resource access.
    - Refactored `Stats` class in `stats.py` to improve data handling and removed unused methods for cleaner code.
    - Improved data visualization in `Lenses.py` by adjusting color gradients and filtering out zero counts in performance displays.
    - Added functionality to track bottom-performing symbols in `viz.py` for a more comprehensive performance analysis.

--- src/stats.py
+++ src/stats.py
@@ -10,12 +10,12 @@ class Stats:
 
     Attributes:
         df (pd.DataFrame): The input DataFrame containing financial data.
+        df_pct_changes (pd.DataFrame): The percentage changes of the assets.
         final_return (pd.Series): The final return of each asset in the DataFrame.
         avg_return (float): The average return of the assets.
 
     Methods:
         ratios: Computes the squared ratios of non-zero final returns.
-        alternative_return: Calculates the alternative return using the computed ratios.
         return_matrix: Generates a matrix of returns based on rolling periods.
         weighted_return_series: Computes a weighted return series.
         weighted_mean_std: Calculates the weighted mean and standard deviation of returns.
@@ -28,9 +28,10 @@ class Stats:
         Args:
             df (pd.DataFrame): The input DataFrame containing financial data.
         """
-        self.df = df
+        self.df_values = df
+        self.df_pct_changes = df.iloc[:, 1:].fillna(1).pct_change().dropna()
         self.final_return = df.iloc[-1, 1:] - 1
-        self.df.fillna(1, inplace=True)
+        self.df_values.fillna(1, inplace=True)
         with pd.option_context("future.no_silent_downcasting", True):
             self.final_return = self.final_return.fillna(0).infer_objects(copy=False)
         self.avg_return = self.final_return.mean()
@@ -40,79 +41,12 @@ class Stats:
         Computes the squared ratios of non-zero final returns.
 
         Args:
-            transformation (Callable[[float], float]): A function to transform the returns. 
+            transformation (Callable[[float], float]): A function to transform the returns.
                                                      Defaults to exponential function.
 
         Returns:
             List[float]: A list of squared ratios.
         """
-        none_zero_ss = self.final_return[self.final_return > 0].apply(transformation).sum()
-        ratios = [transformation(x) / none_zero_ss if x > 0 else 0 for x in self.final_return]
+        ss = self.final_return.apply(transformation).abs().sum()
+        ratios = [transformation(x) / ss for x in self.final_return]
         return ratios
-
-    def alternative_return(self, ratios: List[float] = None) -> float:
-        """
-        Calculate the alternative return using the computed ratios.
-
-        This method computes the alternative return by taking the dot product of the final returns
-        and the provided ratios. If no ratios are provided, it defaults to using the precomputed
-        ratios from the `ratios` property.
-
-        Args:
-            ratios (List[float], optional): A list of ratios to use for the calculation. If not provided,
-                                            the method will use the default ratios.
-
-        Returns:
-            float: The calculated alternative return value.
-        """
-        if ratios is None:
-            ratios = self.ratios
-        return np.dot(self.final_return, ratios)
-
-    @property
-    def return_matrix(self) -> pd.DataFrame:
-        """
-        Generates a matrix of returns based on rolling periods.
-
-        Returns:
-            pd.DataFrame: A DataFrame representing the return matrix.
-        """
-        return (
-            self.df.iloc[:, 1:]
-            .rolling(2)
-            .apply(lambda x: x.iloc[1] / x.iloc[0] - 1)
-            .dropna()
-            .reset_index(drop=True)
-        )
-
-    def weighted_return_series(
-        self,
-        weights: List[float] = None,
-    ) -> pd.Series:
-        """
-        Computes a weighted return series.
-
-        Args:
-            weights (List[float], optional): A list of weights for the assets. Defaults to equal weights.
-
-        Returns:
-            pd.Series: A series representing the weighted returns.
-        """
-        if weights is None:
-            weights = np.ones(self.df.shape[1] - 1) / (self.df.shape[1] - 1)
-        return self.return_matrix.dot(weights)
-
-    def weighted_mean_std(self, weights: List[float] = None) -> Tuple[float, float]:
-        """
-        Calculates the weighted mean and standard deviation of returns.
-
-        Args:
-            weights (List[float], optional): A list of weights for the assets. Defaults to equal weights.
-
-        Returns:
-            Tuple[float, float]: A tuple containing the weighted mean and standard deviation.
-        """
-        return (
-            self.weighted_return_series(weights).mean(numeric_only=True, skipna=True),
-            self.weighted_return_series(weights).std(numeric_only=True, skipna=True),
-        )
